cbf396595d049876e0a2593c447b91ebed6c437c
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2018-04-22 21:55:36 +0200

    Little test changes

---
+++ app/src/test/java/xyz/arnau/muvicat/AppExecutorsTest.kt
@@ -0,0 +1,29 @@
+package xyz.arnau.muvicat
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import java.util.concurrent.Executor
+
+class AppExecutorsTest {
+    private val diskIO = mock(Executor::class.java)
+    private val networkIO = mock(Executor::class.java)
+    private val mainThread = mock(Executor::class.java)
+    private val appExecutors = AppExecutors(diskIO, networkIO, mainThread)
+
+    @Test
+    fun diskIOReturnsExecutor() {
+        assertEquals(diskIO, appExecutors.diskIO())
+    }
+
+    @Test
+    fun networkIOReturnsExecutor() {
+        assertEquals(networkIO, appExecutors.networkIO())
+    }
+
+    @Test
+    fun mainThreadReturnsExecutor() {
+        assertEquals(mainThread, appExecutors.mainThread())
+    }
+}
