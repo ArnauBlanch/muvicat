6a1e7e34d945c688aecae2df050d1516fdec9be5
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2018-04-26 02:50:32 +0200

    #3 - some more tests + coverage exclusions updated

--- app/src/main/java/xyz/arnau/muvicat/utils/DateFormatter.kt
+++ app/src/main/java/xyz/arnau/muvicat/utils/DateFormatter.kt
@@ -17,27 +17,26 @@ import javax.inject.Singleton
 @Singleton
 class DateFormatter @Inject constructor(private val context: Context) {
 
-    fun shortDate(d: Date?): String? {
+    fun shortDate(d: Date?, today: LocalDate = LocalDate.now()): String? {
         if (d == null) {
             return null
         }
         val date = LocalDate(d)
-        val today = LocalDate.now()
         val releaseString = context.getString(R.string.release)
-        when {
+        return when {
             date.isEqual(today) -> { // today
                 val todayString = context.getString(R.string.today)
-                return String.format(releaseString, todayString)
+                String.format(releaseString, todayString)
             }
             date.isEqual(today.plusDays(1)) -> { // tomorrow
                 val tomorrowString = context.getString(R.string.tomorrow)
-                return String.format(releaseString, tomorrowString)
+                String.format(releaseString, tomorrowString)
             }
             date.isAfter(today) -> {
                 val dateString = date.toString("EE dd/MM", Locale("ca"))
-                return String.format(releaseString, dateString)
+                String.format(releaseString, dateString)
             }
-            else -> return null
+            else -> null
         }
     }
 }
