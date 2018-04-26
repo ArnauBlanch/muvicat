f369306b1fb24d77a3854553c2ed2ee54891dee9
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2018-04-26 03:05:42 +0200

    Firebase Analytics & Crashlytics configuration done

--- app/src/main/java/xyz/arnau/muvicat/ui/MainActivity.kt
+++ app/src/main/java/xyz/arnau/muvicat/ui/MainActivity.kt
@@ -10,14 +10,21 @@ import dagger.android.support.HasSupportFragmentInjector
 import xyz.arnau.muvicat.R
 import xyz.arnau.muvicat.ui.movielist.MovieListFragment
 import javax.inject.Inject
+import com.google.firebase.analytics.FirebaseAnalytics
+
+
 
 class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
 
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
 
         supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MovieListFragment())
