3d4b07c29ed0fe5eee5694e84508db8ea680f1b7
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2018-04-29 17:04:53 +0200

    #48 - bottom navigation added + viewpager

--- app/src/main/java/xyz/arnau/muvicat/ui/MainActivity.kt
+++ app/src/main/java/xyz/arnau/muvicat/ui/MainActivity.kt
@@ -7,6 +7,8 @@ import com.google.firebase.analytics.FirebaseAnalytics
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
+import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
 import xyz.arnau.muvicat.R
 import xyz.arnau.muvicat.ui.movie.MovieListFragment
 import javax.inject.Inject
@@ -18,15 +20,39 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
 
     private lateinit var firebaseAnalytics: FirebaseAnalytics
 
+    private val viewPagerAdapter = ViewPagerAdapter(listOf(
+        MovieListFragment(),
+        Fragment()
+    ), supportFragmentManager)
+
+    private val FRAG_MOVIES = 0
+    private val FRAG_CINEMAS = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         firebaseAnalytics = FirebaseAnalytics.getInstance(this)
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.container, MovieListFragment())
-            .commitAllowingStateLoss()
+        setupBottomNavigation()
+
+        fragmentsViewPager.adapter = viewPagerAdapter
+    }
+
+    private fun setupBottomNavigation() {
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.action_movies -> {
+                    fragmentsViewPager.setCurrentItem(FRAG_MOVIES, false)
+                    true
+                }
+                R.id.action_cinemas -> {
+                    fragmentsViewPager.setCurrentItem(FRAG_CINEMAS, false)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     override fun supportFragmentInjector(): AndroidInjector<Fragment> {
