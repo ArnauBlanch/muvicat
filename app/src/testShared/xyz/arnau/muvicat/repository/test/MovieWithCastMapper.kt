42fdca50fe207170aec6f970528f05f6b5f0b535
Arnau Blanch Cortès <arnaubc@gmail.com>
2018-05-19 15:57:46 +0200

    #14 remote+repository+cache layer done & tested

---
+++ app/src/testShared/xyz/arnau/muvicat/repository/test/MovieWithCastMapper.kt
@@ -0,0 +1,17 @@
+package xyz.arnau.muvicat.repository.test
+
+import xyz.arnau.muvicat.cache.model.MovieEntity
+import xyz.arnau.muvicat.repository.model.Movie
+
+object MovieMapper {
+    fun mapFromMovieEntity(movie: MovieEntity) =
+        Movie(
+            movie.id, movie.title, movie.originalTitle, movie.year, movie.direction,
+            movie.cast, movie.plot, movie.releaseDate, movie.posterUrl, movie.priority,
+            movie.originalLanguage, movie.ageRating, movie.trailerUrl,
+            movie.tmdbId, movie.runtime, movie.genres, movie.backdropUrl, movie.voteAverage, movie.voteCount
+        )
+
+    fun mapFromMovieEntityList(movieList: List<MovieEntity>) =
+        movieList.map { mapFromMovieEntity(it) }
+}
